304711a3cb5c6f029357ea3650c08db8f4a02426
Lavish-Patidar <lavishpatidar9926@gmail.com>
2024-07-26 13:46:23 +0530

    changes

--- program1.js
+++ program1.js
@@ -2,6 +2,38 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here
+  if (!grid || grid.length === 0) {
+    return 0;
+}
+
+const rows = grid.length;
+const cols = grid[0].length;
+let islands = 0;
+
+const DepthFirstSearch = (i, j) => {
+    if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] !== 'L') {
+        return;
+    }
+
+    grid[i][j] = '#'; 
+
+    
+    DepthFirstSearch(i + 1, j);
+    DepthFirstSearch(i - 1, j);
+    DepthFirstSearch(i, j + 1);
+    DepthFirstSearch(i, j - 1);
+};
+
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+        if (grid[i][j] === 'L') {
+            DepthFirstSearch(i, j);
+            islands++;
+        }
+    }
+}
+
+return islands;
 
 };
 
--- program2.js
+++ program2.js
@@ -1,7 +1,31 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+  let j = 0;
+  let i = 0;
 
-  };
-  
-  module.exports = decodeTheRing;
+  while (j < s.length && i < p.length) {
+    if (p[i] === '*') {
+
+      if (i === p.length - 1) return true;
+
+      
+      let nextChar = p[i + 1];
+      let NextCharOfIndex = s.indexOf(nextChar, j);
+
+      if (NextCharOfIndex === -1) return false;
+
+      j = NextCharOfIndex;
+      i++;
+    } else if (p[i] === '?' || p[i] === s[j]) {
+      j++;
+      i++;
+    } else {
+      return false;
+    }
+  }
+
+  return j === s.length && i === p.length;
+
+};
+
+module.exports = decodeTheRing;
